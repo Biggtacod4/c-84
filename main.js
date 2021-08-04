f552955451dd70747ac73c63b9dc2b9469cccf46
Biggtacod4 <81197211+Biggtacod4@users.noreply.github.com>
2021-08-04 15:34:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+canvas=document.getElementById('myCanvas');
+ctx=canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); 
+	img_imgTag.onload = uploadimg;
+	img_imgTag.src = img_image;   
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{}
+	else{
+		otherkey();
+	document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	console.log("otherkey")
+	}
+	if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+	{}
+else{
+	specialkey();
+	document.getElementById("d1").innerHTML="You pressed special key";
+	console.log("specialkey")
+}
+if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+{}
+else{
+	aplhabetkey();
+document.getElementById("d1").innerHTML="You pressed aplhabetkey";
+console.log("alphabetkey")
+}
+if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+{}
+else{
+	numberkey();
+document.getElementById("d1").innerHTML="You pressed numberkey";
+console.log("numberkey")
+}
+if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+{}
+else{
+	arrowkey();
+document.getElementById("d1").innerHTML="You pressed arrowkey";
+console.log("arrowkey")
+}
+}
+
+function aplhabetkey()
+{
+	img_image="Alpkey.png";
+	add();
+
+}
+function numberkey()
+{
+	img_image="numkey.png";
+	add();
+}
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
